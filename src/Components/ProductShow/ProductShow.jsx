72747710e8794d152137f422bb22003c2ca3540f
FahimHasan683031 <fahimhasan683031@gmail.com>
2023-10-19 16:39:21 +0600

    Product single card design complite

--- src/Components/ProductShow/ProductShow.jsx
+++ src/Components/ProductShow/ProductShow.jsx
@@ -1,12 +1,17 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import ProductSingle from "./ProductSingle";
+
 
 
 const ProductShow = () => {
-    const data = useLoaderData()
-    console.log(data)
+    const products = useLoaderData()
     return (
-        <div>
-            this is product show page
+        <div className="max-w-screen-xl mx-auto my-16 px-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                {
+                  products?.map(product=><ProductSingle key={product._id} product={product}></ProductSingle>)  
+                }
+            </div>
         </div>
     );
 };
